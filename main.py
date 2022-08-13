27e599fccc888bd5df988a84b4abf37a62a787f8
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-08-13 15:29:22 -0500

    👨‍🏭 Add exception manager

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dic = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-word_list = [phonetic_dic[letter] for letter in word]
-print(word_list)
+
+def generate_code():
+    word = input("Enter a word: ").upper()
+    try:
+        word_list = [phonetic_dic[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet are allowed.")
+        generate_code()
+    else:
+        print(word_list)
+
+
+generate_code()
